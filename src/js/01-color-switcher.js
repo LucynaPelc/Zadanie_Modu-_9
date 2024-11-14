e00127c2ffb64c0a0f1c44941eecb05b4d7a9495
LucynaPelc <lucynapelc.project@gmail.com>
2024-11-14 11:08:58 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const button = document.querySelector(".button");
+button.style.fontSize = "24px";
+button.style.textAlign = "center";
+console.log(button.style);
 
+
+
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+       
+function changeBackGroundColor () {
+
+    document.body.style.backgroundColour = getRandomHexColor();
+    setTimeout(changeBackGroundColor, 1000);
+    console.log(changeBackGroundColor);
+}
+
+{/* <button type="button" data-start>Start</button>
+    <button type="button" data-stop>Stop</button> */}
+
+    const startButton = document.querySelector('button[data-start="start"]');
+        button.addIventListener('click', function() {
+        this.disabled = true;
+        document.querySelector('button[data-stop="stop"]'); disabled = false;
+        changeBackGroundColor();
+    });   
+console.log(startButton.dataset.start);
+  
+    const stopButton = document.querySelector('button[data-stop="staop"]');
+        button.addIventListener('click', function() {
+        this.disabled = true;
+        document.querySelector('button[data-start="start"]'); disabled = false;
+        changeBackGroundColor();
+    });   
+console.log(stopButton.dataset.stop);
+    
