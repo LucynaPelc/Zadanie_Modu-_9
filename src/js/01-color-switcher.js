0a1be5c53a5a40c4b5b3e6ebd22957737921000d
LucynaPelc <lucynapelc.project@gmail.com>
2024-11-20 09:22:33 +0100

    zmiana3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,14 @@
-// const startButton = document.querySelector('button[data-start]');
-// startButton.style.fontSize = "100px";
-// startButton.style.textAlgin = "center";
-// console.log(startButton)
-
-// const stopButton = document.querySelector('button[data-stop]');
-// stopButton.style.fontSize = "100px";
-// stopButton.style.textAlgin = "center";
-// console.log(stopButton)
-
-
-
 function getRandomHexColor() {  
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }
+}
        
 function changeBackGroundColor () {
-
     document.body.style.backgroundColor = getRandomHexColor();
-    setInterval(changeBackGroundColor, 1000);
-    console.log(changeBackGroundColor);
-}
+    setInterval(changeBackGroundColor, 1000); 
+
+function stopChangeBackGroundColor()  {
+    clearInterval(changeBackGroundColor) ;
+}}
 
 {/* <button type="button" data-start>Start</button>
     <button type="button" data-stop>Stop</button> */}
@@ -30,13 +19,10 @@ function changeBackGroundColor () {
         document.querySelector('button[data-stop]'); stopButton.disabled = false;
         changeBackGroundColor();
     });   
-console.log(startButton.dataset.start);
-  
     const stopButton = document.querySelector('button[data-stop]');
-        stopButton.addEventListener('click', function() {
-        this.disabled = true;
-        document.querySelector('button[data-start]'); disabled = false;
-        changeBackGroundColor();
-    });   
-console.log(stopButton.dataset.stop);
+        stopButton.addEventListener('click', function() {         
+        stopButton.disabled = true;
+        document.querySelector('button[data-start]'); startButton.disabled = false;   
+        changeBackGroundColor(); 
+    });    
     
